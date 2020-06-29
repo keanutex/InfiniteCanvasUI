bf302fb8191239aab434ee7c53d43e89739a92e1
Mzee Sotsaka <mzwandile@bbd.co.za>
2020-06-29 13:53:01 +0200

    Added reg and sign in - need to handle incorrect password

--- infinitecanvas/src/Components/login.js
+++ infinitecanvas/src/Components/login.js
@@ -1,41 +1,112 @@
-import React, {useState} from 'react';
-import {Form, Input, Button, Buttons} from './Styled-Components/styled-components';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Form, Input, Button } from './Styled-Components/styled-components';
+import { setUserSession } from './authService.js';
 
-function Login(props) {
-    const [formData, setFormData] = useState({});
+class Authentication extends React.Component {
+    emailChanged = (event) => {
+        this.setState({ email: event.target.value })
+    }
+    usernameChanged = (event) => {
+        this.setState({ username: event.target.value })
+    }
+    passwordChanged = (event) => {
+        this.setState({ password: event.target.value })
+    }
 
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
+    handleSubmitLogin = async (e) => {
+        console.log(this.state.password, this.state.username);
+        e.preventDefault();
+       await axios.post('http://127.0.0.1:3000/users/signin',
+            { 
+                username: this.state.username, 
+                password: this.state.password 
+            },
+            {
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+                }
+            }
+        ).then(response => {
+            if (response.status == 200) {
+                this.setState({ authLevel: 1 });
+            }
+            else {
+                this.setState({ authLevel: 0 }).catch(error => {
+                });
+            };
         });
-    };
-
-    const handleSubmitLogin = (e) => {
+    }
+    handleSubmitRegister = async (e) => {
         e.preventDefault();
-        // here is where you would login the user
-        console.log(formData);
-        props.setUser(formData.username);
+        await axios.post('http://127.0.0.1:3000/users/register',
+            {
+                username: this.state.username,
+                password: this.state.password,
+                email: this.state.email
+            },
+            {
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+                }
+            }).then(response => {
+                console.log(response);
+                setUserSession(response.data.token, response.data.user);
+                console.log(response);
+                this.setState({ authLevel: 2 })
+            }).catch(error => {
+            });
     };
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            email: '',
+            password: '',
+            authLevel: 0
+        }
+    }
+    render() {
+        if (this.state.authLevel == 0)
+            return <div>
+                {
+                    <Form>
+                        <Input type="text" onChange={this.usernameChanged} placeholder="Username" required />
+                        <Input type="password" onChange={this.passwordChanged} placeholder="Password" required />
+                        <Button type="button" onClick={this.handleSubmitLogin}>Login</Button>
 
-    const handleSubmitRegister = (e) => {
-        e.preventDefault();
-        // here is where you would register the user
-        console.log(formData);
-        props.setUser(formData.username);
-    };
+                        <Input type="text" onChange={this.usernameChanged} placeholder="Username" required />
+                        <Input type="email" onChange={this.emailChanged} placeholder="Email" required />
+                        <Input type="password" onChange={this.passwordChanged} placeholder="Password" required />
+                        <Button type="button" onClick={this.handleSubmitRegister}>Register</Button>
+                    </Form>
+                }
 
-    return (
-        <Form>
-            <Input type="text" name="username" onChange={handleChange} placeholder="Username"/>
-            <Input type="password" name="password" onChange={handleChange} placeholder="Password"/>
-            <Buttons>
-                <Button type="button" onClick={handleSubmitLogin}>Login</Button>
-                <p> or </p>
-                <Button type="button" onClick={handleSubmitRegister}>Register</Button>
-            </Buttons>
-        </Form>
-    )
+            </div>
+        else if (this.state.authLevel == 1)
+            return <div>
+                {
+
+                    <Form>
+                        <h1 type="text" style={{textAlign: 'center'}}> <div>Welcome back</div>{this.state.username}</h1>
+                        <Button type="button" onClick={this.handleSubmitLogin}>Continue</Button>
+                    </Form>
+                }
+
+            </div>
+        else if (this.state.authLevel == 2)
+            return <div>
+                {
+                    <Form>
+                        <h1 type="text" style={{textAlign: 'center'}}> Great to have you join us,{this.state.username}</h1>
+                        <Button type="button" onClick={this.handleSubmitLogin}>Continue</Button>
+                    </Form>
+                }
+
+            </div>
+    }
+
 }
-
-export default Login
+export default Authentication
