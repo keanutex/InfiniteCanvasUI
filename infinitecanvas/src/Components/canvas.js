666d11cdabbd7fd738d7ed78e114906712b2164f
labuschagnei <ian.labuschagne@vcontractor.co.za>
2020-07-08 22:57:14 +0200

    added code for serverside event on canvas

--- infinitecanvas/src/Components/canvas.js
+++ infinitecanvas/src/Components/canvas.js
@@ -1,13 +1,21 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {CanvasArea} from './Styled-Components/styled-components';
 
 function Canvas() {
+    const [newPix, seNewPix] = useState('')
+
     useEffect(() => {
-        const canvas = document.getElementById("myCanvas");
-        const ctx = canvas.getContext('2d');
-        ctx.beginPath()
-        ctx.strokeRect(50, 35, 50, 50)
-    });
+        
+
+        let eventSource = new EventSource("http://localhost:3000/canvas/getNew")
+        eventSource.onmessage = e => updatePixelData(JSON.parse(e.data))
+    }, []);
+
+    const updatePixelData = (data) => {
+        seNewPix(data);
+    }
+
+    console.log(newPix)
     
     return (
         <CanvasArea id="myCanvas"></CanvasArea>
